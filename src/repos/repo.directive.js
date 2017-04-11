9c6252e39ee1fa1a9316a04c2f58eaa25a7cf444
Carlyse Jordan <carlyse.jordan@Carlyses-MacBook-Air.local>
2017-04-10 21:40:05 -0400

    can populate with repos

---
+++ src/repos/repo.directive.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+    angular.module('gh').directive('repo', repo);
+
+    function repo(){
+      return {
+        templateUrl: 'repos/repo.template.html',
+        restrict: 'E'
+      };
+    }
+}());
